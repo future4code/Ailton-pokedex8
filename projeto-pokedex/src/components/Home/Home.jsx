d0175f755f6eb7e99dc4adb9a86b924e82af424d
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-07-22 14:30:02 -0300

    ZANG PICUDO

--- projeto-pokedex/src/components/Home/Home.jsx
+++ projeto-pokedex/src/components/Home/Home.jsx
@@ -4,34 +4,17 @@ import { Card } from "../Card/Card";
 import { ContainerGeral, Title } from "./HomeStyle";
 import { useRequestData } from "../../hooks/useRequestData";
 import { ChangePage } from "../ChangePage/ChangePage";
-import axios from "axios";
-import { baseUrl } from "../../constants/url";
-export const Home = () => {
-  const [pokemon, setPokemon] = useState({});
-  const takePokemon = async (start, quantity) => {
-    try {
-      const res = await axios.get(
-        `${baseUrl}/?offset=${start}&limit=${quantity}`
-      );
-      setPokemon(res.data);
-    } catch (error) {
-      alert(error);
-    }
-  }; 
 
+export const Home = () => {
   const [page, setPage] = useState(0);
-  // const pokemon = useRequestData(page * 24, 24);
+  const pokemon = useRequestData(page * 24, 24);
   const choosePage = () => {
     setPage(page + 1);
   };
-  useEffect(() => {
-    takePokemon(page * 24, 24);
-  }, [pokemon])
-
   return (
     <>
       <HeaderComp showing1={false} showing2={true} />
-      <ContainerGeral onClick={choosePage}>
+      <ContainerGeral>
         <Title>Todos os Pokemons</Title>
         {pokemon.results?.map(({ name }) => {
           return <Card key={name} pokemon={name} />;
