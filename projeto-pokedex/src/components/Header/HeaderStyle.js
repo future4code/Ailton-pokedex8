28e3ef6bcc0cf122fd6b807db35470a0cdb011c7
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-07-24 20:56:54 -0300

    domingo

--- projeto-pokedex/src/components/Header/HeaderStyle.js
+++ projeto-pokedex/src/components/Header/HeaderStyle.js
@@ -6,9 +6,16 @@ export const Header = styled.header`
   align-items: center;
   width: 100%;
   height: 160px;
+  @media (max-width: 950px) {
+    flex-direction: column;
+    height: 350px;
+  }
 `;
 export const AllPokemonsDiv = styled.div`
   width: 30%;
+  @media (max-width: 950px) {
+    width: 100%;
+  }
 `;
 export const AllPokemons = styled.p`
   display: flex;
@@ -22,10 +29,16 @@ export const AllPokemons = styled.p`
   :hover {
     text-decoration: underline;
   }
+  @media (max-width: 950px) {
+    font-size: 1.5rem;
+  }
 `;
 export const Logo = styled.img`
   width: 308px;
   height: 114px;
+  @media (max-width: 950px) {
+    margin-top: 30px;
+  }
 `;
 export const PokedexButton = styled.button`
   width: 288px;
@@ -42,6 +55,11 @@ export const PokedexButton = styled.button`
   :hover {
     background: #0a84db;
   }
+  @media (max-width: 950px) {
+    font-size: 1.5rem;
+    width: 200px;
+    height: 60px;
+  }
 `;
 export const PokedexButtonDiv = styled.div`
   display: flex;
@@ -49,4 +67,7 @@ export const PokedexButtonDiv = styled.div`
   align-items: center;
   width: 30%;
   height: 100%;
+  @media (max-width: 950px) {
+    height: 50%;
+  }
 `;
